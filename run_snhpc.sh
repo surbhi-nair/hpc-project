3cc24af3d226897b4de2372a4b3e2f14f6faed06
Surbhi Nair <59994225+surbhi-nair@users.noreply.github.com>
2025-07-10 23:01:45 +0200

    batch script bwunicluster
    batch script to run the python script on bwunicluster

---
+++ run_snhpc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=gpu_h100
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:30:00
+#SBATCH --job-name=pytorch_test
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4  # For DataLoader's num_workers
+
+# Load conda (adjust module name if needed)
+module load devel/miniforge/24.11.0-python-3.12
+
+# Activate env (assuming default location)
+conda activate hpcenv
+
+# Navigate to repo (if not already there)
+cd ~/hpc-project
+
+# Run script (paths are now relative to repo root)
+python src/pythontest.py
