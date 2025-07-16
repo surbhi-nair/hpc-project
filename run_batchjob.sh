e65b0d3a073a34b8b79acb6201bf514f26d983fe
Surbhi Nair <surbhinair99@gmail.com>
2025-07-16 23:04:37 +0200

    code cleanup

--- run_batchjob.sh
+++ run_batchjob.sh
@@ -29,7 +29,7 @@ echo "Running from: $(pwd)"
 cd ~/hpc-project
 
 # # Run script (paths are now relative to repo root)
-python src/milestones/milestone5-old.py
+python src/milestones/milestone5.py
 
 # # Run with unbuffered output
 # PYTHONUNBUFFERED=1 python src/pythontest.py
